cdb3d1b3a241917a8c29c33ff4b79901348784e0
zionawit <tsionget22@gmail.com>
2023-11-02 22:17:31 -0700

    modify

--- javaaa.java
+++ javaaa.java
@@ -46,6 +46,6 @@ class Person {
     }
 
     public void sayHello() {
-        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+        System.out.println("Hello, my name is tsion getnet " and I am " + age + " years old.");
 }
     }
